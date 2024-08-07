9d5b8028e82d51637db4fb76026980135f699310
Janith Hansidu Yapa <janithhansiduyapa1@gmail.com>
2024-08-07 21:07:19 +0530

    Update Queen's_Attack_II.py

--- 14-Queen's Attack II/Queen's_Attack_II.py
+++ 14-Queen's Attack II/Queen's_Attack_II.py
@@ -1,50 +1,45 @@
 def queensAttack(n, k, r_q, c_q, obstacles):
-    # Write your code here
-    # Directions of queen can move
-    squares = 0
-    directions = [(1.0, 1.0), (1.0, 0.0), (0.0, 1.0), (1.0, -1.0), (0.0, -1.0), (-1.0, -1.0), (-1.0, 0), (-1.0, 1.0)]
+    # number of positions that queen can move in board
+    max_reach ={
+        "up": n - r_q,
+        "up_right": min(n-r_q, n-c_q),
+        "right": n-c_q,
+        "down_right": min(r_q-1,n-c_q),
+        "down": r_q-1,
+        "down_left": min(r_q-1, c_q-1),
+        "left": c_q-1,
+        "up_left": min(n-r_q, c_q-1)
+    }
 
-    # iterate trough obstacles and find whether obstacles block queen
-    for obj in obstacles:
-        try:
-            row, column = obj
-            row = row - r_q
-            column = column - c_q
-            # print(row,column)
-            # if it removes from the directions that is the squares till that point
-            max_num = max(abs(column), abs(row))
+    # with the obstacle update the max reach
+    for obs_r,obs_c in obstacles:
+        row_dif = obs_r - r_q
+        col_dif = obs_c - c_q
 
-            # Divide the number column and row of the max of the numbers to find how many sqares
-            find_row = row / max_num
-            find_column = column / max_num
+        if row_dif == 0:
+            if col_dif > 0:
+                max_reach["right"] = min(max_reach["right"], col_dif-1)
+            else:
+                max_reach["left"] = min(max_reach["left"], -col_dif-1)
 
-            directions.remove((find_row, find_column))
-            # print(row, column)
+        if col_dif == 0:
+            if row_dif > 0:
+                max_reach["up"] = min(max_reach["up"], row_dif-1)
+            else:
+                max_reach["down"] = min(max_reach["down"], -row_dif-1)
 
-            # update the squares
-            squares = squares + int(max(abs(row)-1, abs(column)-1))
-            # print(squares)
-        except:
-            # print("a")
-            pass
+        elif abs(row_dif) == abs(col_dif):
+            if row_dif > 0 and col_dif > 0:
+                max_reach["up_right"] = min(max_reach["up_right"], row_dif - 1)
+            elif row_dif > 0 > col_dif:
+                max_reach["up_left"] = min(max_reach["up_left"], row_dif - 1)
+            elif row_dif < 0 < col_dif:
+                max_reach["down_right"] = min(max_reach["down_right"], -row_dif - 1)
+            elif row_dif < 0 and col_dif < 0:
+                max_reach["down_left"] = min(max_reach["down_left"], -row_dif - 1)
 
-    print(directions)
-
-    for direction in directions:
-        x_move, y_move = direction
-        x_center = r_q
-        y_center = c_q
-        # print(x_center,y_center,"--")
-        # while column in range 1.n
-        while 1 < x_center < n and 1 < y_center < n:
-            x_center = x_center + x_move
-            y_center = y_center + y_move
-            squares = squares + 1
-            # print(x_center,y_center)
-
-    # print(squares)
-    return squares
+    return sum(max_reach.values())
 
 
 if __name__ == '__main__':
-    queensAttack(5, 3, 4, 3, [[5, 5], [4, 2], [2, 3]])
+    print(queensAttack(5, 3, 4, 3, [[5, 5], [4, 2], [2, 3]]))
