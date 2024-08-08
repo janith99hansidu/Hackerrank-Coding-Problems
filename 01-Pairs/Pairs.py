cbfde150a445c4b65f616ad4056fce361801f450
Janith Hansidu Yapa <janithhansiduyapa1@gmail.com>
2024-08-08 09:05:35 +0530

    Update Pairs.py

--- 01-Pairs/Pairs.py
+++ 01-Pairs/Pairs.py
@@ -1,11 +1,18 @@
 # brute force method
 def pairs(k, arr):
-    num_times = 0
-    for i in range(len(arr)-1):
-        for j in range(i+1,len(arr)):
-            if(abs(arr[i]-arr[j]) == k):
-                num_times = num_times + 1
-    return num_times
+    # make a list of unique values set
+    arr_set = set(arr)
+    count = 0
+    
+    for num in arr:
+        # checking num + k in the list get average o(1) time complexity 
+        if (num + k) in arr_set:
+            count += 1
+        # checking num + k in the list get average o(1) time complexity
+        if (num - k) in arr_set:
+            count += 1
+    
+    return count // 2
 
 if __name__ == '__main__':
     print(pairs(2, [1, 5, 3]))
