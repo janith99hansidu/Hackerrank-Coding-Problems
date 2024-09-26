1ae330adbad40b7b038c05b93bdced87bd5e7678
Janith Hansidu Yapa <janithhansiduyapa1@gmail.com>
2024-09-26 14:14:51 +0530

    Create Student_Attendance_Record_II_Recursion.py

---
+++ 103-Student Attendance Record II/Student_Attendance_Record_II_Recursion.py
@@ -0,0 +1,29 @@
+def checkRecord(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+
+    # helper function to recursively check all possibilities
+    def helper(current_n, consecutive_late, total_absents):
+        # End case for exceeding the allowed consecutive late days or absences
+        if consecutive_late == 3 or total_absents == 2:
+            return 0
+
+        # Base case: all days are processed
+        if current_n == n:
+            return 1
+
+        # Recursive calls for all possible records
+        total = (helper(current_n + 1, 0, total_absents)  # 'P': Present
+                 + helper(current_n + 1, consecutive_late + 1, total_absents)  # 'L': Late
+                 + helper(current_n + 1, 0, total_absents + 1))  # 'A': Absent
+
+        return total
+
+    return helper(0, 0, 0)
+
+
+if __name__ == '__main__':
+    n = 2
+    print(checkRecord(n))
