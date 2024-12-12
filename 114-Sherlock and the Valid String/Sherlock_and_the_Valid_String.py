20a74e596b70f73533d7447f3b4971db7e998986
Janith Hansidu Yapa <janithhansiduyapa1@gmail.com>
2024-12-12 23:56:24 +0530

    Update Sherlock_and_the_Valid_String.py

--- 114-Sherlock and the Valid String/Sherlock_and_the_Valid_String.py
+++ 114-Sherlock and the Valid String/Sherlock_and_the_Valid_String.py
@@ -1,7 +1,21 @@
 from collections import Counter
 
 def isValid(s):
+    # count the number of occurrences 
+    char_count = Counter(s)
+    # print(char_count)
     
+    # count frequencies of each 
+    # there is 2 possible to the condition for correct
+    # if the larger frequency count is more than one from the lower one
+    # if the lower frequency one occur only once
+    freq_count = Counter(char_count.values())
+    
+    if len(freq_count) == 1:
+        # all characters have the same frequency
+        return "YES"
+    elif len(freq_count) == 2:
+        
 
     
 if __name__ == '__main__':
