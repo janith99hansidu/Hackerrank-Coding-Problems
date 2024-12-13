1f813673fa616b36148e026d6270186bfe2a3333
Janith Hansidu Yapa <janithhansiduyapa1@gmail.com>
2024-12-13 22:47:12 +0530

    Update Sherlock_and_the_Valid_String.py

--- 114-Sherlock and the Valid String/Sherlock_and_the_Valid_String.py
+++ 114-Sherlock and the Valid String/Sherlock_and_the_Valid_String.py
@@ -15,8 +15,18 @@ def isValid(s):
         # all characters have the same frequency
         return "YES"
     elif len(freq_count) == 2:
+        # make the conditions otherwise return NO
+        freq_1, count_1 = freq_count.most_common()[0]
+        freq_2, count_2 = freq_count.most_common()[1]
         
-
+        # if there is only one element in with count 1
+        if (freq_1 == 1 and count_1 == 1) or (freq_2 == 1 and count_2 == 1):
+            return "YES"
+        # check if we can remove one occurrence of the higher frequency
+        elif abs(freq_1 - freq_2) == 1 and (count_1 == 1 or count_2 == 1):
+            return "YES"
+        
+    return "NO"
     
 if __name__ == '__main__':
     
