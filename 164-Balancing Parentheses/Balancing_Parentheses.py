1671f5846eae2a3af5d5b524e0b0ea028aafd82f
Janith Hansidu Yapa <97206910+janith99hansidu@users.noreply.github.com>
2025-02-01 21:42:05 +0530

    Create Balancing_Parentheses.py

---
+++ 164-Balancing Parentheses/Balancing_Parentheses.py
@@ -0,0 +1,11 @@
+def Balancing_parentheses(s):
+    # 1. if the string is empty return true
+    # 2. if ( found put to the stack with False
+    # 2. if :( found put to the stack with true [(, True]
+    # 3. if the :) found get the pop the stack and mark as [(,True]
+    # 4. if ) found pop the list if no element to pop return false
+    # 4. get all remaining in stack and check the all are True if one is false return false
+
+    return
+
+if __name__ == "__main__":
